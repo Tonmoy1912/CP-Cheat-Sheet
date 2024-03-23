5643bfd6048b2ddf877947eb266787929aee8516
Tonmoy <tonmoybiswas19122002@gmail.com>
2024-03-23 19:47:59 +0530

    KMP and Z function added

---
+++ kmp.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class KMP{
+    vector<int> getKMP(string &s,int n){
+        vector<int> arr(n);
+        int i=1,j=0;
+        while(i<n){
+            while(j!=0 && s[j]!=s[i]){
+                j=arr[j-1];
+            }
+            if(s[j]==s[i]){
+                j++;
+            }
+            arr[i]=j;
+            i++;
+        }
+        return arr;
+    }
+    public:
+    int kmp(string &str,string &sub){
+        int m=str.size();
+        int n=sub.size();
+        vector<int> arr=getKMP(sub,n);
+        int i=0,j=0;
+        while(i<m){
+            while(j!=0 && str[i]!=sub[j]){
+                j=arr[j-1];
+            }
+            if(str[i]==sub[j]){
+                j++;
+            }
+            if(j==n){
+                return i-(n-1);
+            }
+            i++;
+        }
+        return -1;
+    }
+};
